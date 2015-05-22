544594dbf063385d332c3cfd2239e41b43da834f
ewertonews <ews@cin.ufpe.be>
2015-05-21 23:55:42 -0300

    trial to solve the problem of duplicate and missing item (block)

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = '127.0.0.1';
 $db['default']['username'] = 'root';  
-$db['default']['password'] = 'admin';
+$db['default']['password'] = 'sp2014';
 $db['default']['database'] = 'elena';  
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
